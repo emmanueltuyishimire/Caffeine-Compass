af9c6ea8b5c815174a4ee17a1923afede5e38cb5
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-11-13 19:02:25 +0000

    Try fixing this error: `Console Error: In HTML, <a> cannot be a descenda

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -73,9 +73,9 @@ const Header = () => {
             <NavigationMenu className="hidden md:flex">
                 <NavigationMenuList>
                     <NavigationMenuItem>
-                        <Link href="/" passHref>
-                          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                            <a>Home</a>
+                        <Link href="/" legacyBehavior passHref>
+                          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            Home
                           </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
@@ -130,9 +130,9 @@ const Header = () => {
                         </NavigationMenuContent>
                     </NavigationMenuItem>
                      <NavigationMenuItem>
-                        <Link href="/about" passHref>
-                          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-                            <a>About</a>
+                        <Link href="/about" legacyBehavior passHref>
+                          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                            About
                           </NavigationMenuLink>
                         </Link>
                     </NavigationMenuItem>
