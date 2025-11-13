210a0e8173147bacee5170b1e28d292c967e6377
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-11-13 20:40:50 +0000

    Enhance app accessibility especially color contrast. make darkmode defau

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,16 +4,19 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import AppLayout from '@/components/layout/AppLayout';
+import { SidebarProvider } from '@/components/ui/sidebar';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="dark"
       enableSystem
       disableTransitionOnChange
     >
-      <AppLayout>{children}</AppLayout>
+      <SidebarProvider defaultOpen={false}>
+        <AppLayout>{children}</AppLayout>
+      </SidebarProvider>
       <Toaster />
     </ThemeProvider>
   );
