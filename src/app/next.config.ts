c3273063ba6bf6b066b8f128907a9ebb6d67530c
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-11-16 07:13:23 +0000

    update proxy domain for blog because it is not at blog.calculation.site,

---
+++ src/app/next.config.ts
@@ -0,0 +1,51 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  devIndicators: {
+    allowedDevOrigins: [
+      'https://6000-firebase-studio-1762910981710.cluster-ikslh4rdsnbqsvu5nw3v4dqjj2.cloudworkstations.dev',
+    ],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      }
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/blog',
+        destination: 'https://caffeine-compass-blog.netlify.app/blog',
+      },
+      {
+        source: '/blog/:path*',
+        destination: 'https://caffeine-compass-blog.netlify.app/blog/:path*',
+      },
+      {
+        source: '/business',
+        destination: 'https://business.calculation.site',
+      },
+      {
+        source: '/business/:path*',
+        destination: 'https://business.calculation.site/:path*',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
