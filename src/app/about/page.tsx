3662daa2764f556a87bd480623309fb7daef7d37
Emmanuel TUYISHIMIRE <emmanueltuyishimire766@gmail.com>
2025-11-18 00:15:32 +0000

    use logo.pn and favicon.icofiles in public folder, please

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,4 @@
 
-
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { Users, Target, HeartPulse } from 'lucide-react';
